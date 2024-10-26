8d302ccb8994f7b4e955cda77484ab26ff763923
CianBarrettATU <G00416157@atu.ie>
2024-10-26 14:56:16 +0000

    served a html file

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app =  express();
 const port = 3000;
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 //created url which takes parameters and displays names
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/hello/:name/:secondName', (req, res)=> {
     const name = req.params.name;
     const secondName = req.params.secondName;
